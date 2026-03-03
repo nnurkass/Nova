c7367a869338cdfbc97dc3b39ea9ea8ea759fa0a
nura <kasymkannurasyl@gmail.com>
2026-03-03 18:01:12 +0500

    fix(db): add compare_type=True to alembic context.configure

--- migrations/env.py
+++ migrations/env.py
@@ -37,7 +37,8 @@ target_metadata = Base.metadata
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
     context.configure(url=url, target_metadata=target_metadata,
-                      literal_binds=True, dialect_opts={"paramstyle": "named"})
+                      literal_binds=True, dialect_opts={"paramstyle": "named"},
+                      compare_type=True)
     with context.begin_transaction():
         context.run_migrations()
 
@@ -49,7 +50,8 @@ def run_migrations_online() -> None:
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(connection=connection, target_metadata=target_metadata,
+                          compare_type=True)
         with context.begin_transaction():
             context.run_migrations()
 
